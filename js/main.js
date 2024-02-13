6c416daa454d6e1ddf85e69d2fdaa5867ee76fe5
meni-kr <meni.kramer@gmail.com>
2024-02-13 19:29:02 +0200

    Add max size ball 400

--- js/main.js
+++ js/main.js
@@ -3,12 +3,18 @@
 var gBallCurrSize = 100
 
 function onBallClick(){
-    const addSize = 50
+    if(gBallCurrSize<400){
+     const addSize = 50
     const elBall = document.querySelector('.ball')
     const elBallTxt = document.querySelector('.ball-text')
     elBall.style.height = gBallCurrSize + addSize + 'px'
     elBall.style.width = gBallCurrSize + addSize + 'px'
     gBallCurrSize +=addSize
-    elBallTxt.innerText = gBallCurrSize
+    elBallTxt.innerText = gBallCurrSize   
+    }else{
+        gBallCurrSize = 50
+        onBallClick()
+    }
+    
     
 }
