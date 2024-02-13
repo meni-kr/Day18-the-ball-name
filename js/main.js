7621fe176d2256cca6705826f50be9967ce67410
meni-kr <meni.kramer@gmail.com>
2024-02-13 22:17:09 +0200

    Correct init ball size

--- js/main.js
+++ js/main.js
@@ -2,20 +2,23 @@
 
 var gBallCurrSize = 100
 
-function onBallClick(){
+function onBallClick() {
     const elBall = document.querySelector('.ball')
     const elBallTxt = document.querySelector('.ball-text')
     elBall.style.backgroundColor = getRandomColor()
-    if(gBallCurrSize<400){
-     const addSize = getRandomInt(20, 60)
-    elBall.style.height = gBallCurrSize + addSize + 'px'
-    elBall.style.width = gBallCurrSize + addSize + 'px'
-    gBallCurrSize +=addSize
-    elBallTxt.innerText = gBallCurrSize   
-    }else{
-        gBallCurrSize = 50
-        onBallClick()
+    if (gBallCurrSize < 400) {
+        const addSize = getRandomInt(20, 60)
+        elBall.style.height = gBallCurrSize + addSize + 'px'
+        elBall.style.width = gBallCurrSize + addSize + 'px'
+        gBallCurrSize += addSize
+        elBallTxt.innerText = gBallCurrSize
+    } else {
+        gBallCurrSize = 100
+        elBall.style.height = gBallCurrSize  + 'px'
+        elBall.style.width = gBallCurrSize  + 'px'
+        elBallTxt.innerText = gBallCurrSize
+       
     }
-    
-    
+
+
 }
