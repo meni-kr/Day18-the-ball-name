9c3cbd4886457968db049bd9673894710c7c4842
meni-kr <meni.kramer@gmail.com>
2024-02-13 17:46:45 +0200

    Change the onBallClick function to add 50px to the ball

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 'use strict'
 
+var gBallCurrSize = 100
+
 function onBallClick(){
-    console.log('i am a ball');
+    const addSize = 50
+    const elBall = document.querySelector('.ball')
+    elBall.style.height = gBallCurrSize + addSize + 'px'
+    elBall.style.width = gBallCurrSize + addSize + 'px'
+    gBallCurrSize +=addSize
+    elBall.innerText = gBallCurrSize
+    
 }
