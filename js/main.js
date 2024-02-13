7eec3abd5f279f1385b3be4cde8738ff77bc4b9c
meni-kr <meni.kramer@gmail.com>
2024-02-14 01:49:08 +0200

    Add function on the third ball

--- js/main.js
+++ js/main.js
@@ -2,15 +2,24 @@
 
 var gBall1CurrSize = 100
 var gBall2CurrSize = 100
+var gCurrBall1Color = 'darkkhaki'
+var gCurrBall2Color = 'rgb(186, 111, 230)'
+var gIsBall3Clicked = false
 const gMaxDiameter = 400
 
 
 function onBallClick(elBall) {
     const currClass = '.' + elBall.classList[0]
     const elCurrBall = document.querySelector(currClass)
-    const elBallTxt = document.querySelector(currClass, 'span')
+
     const addSize = getRandomInt(20, 60)
-    elCurrBall.style.backgroundColor = getRandomColor()
+    if (currClass === '.ball') {
+        gCurrBall1Color = getRandomColor()
+        elCurrBall.style.backgroundColor = gCurrBall1Color
+    } else {
+        gCurrBall2Color = getRandomColor()
+        elCurrBall.style.backgroundColor = gCurrBall2Color
+    }
 
     if (currClass === '.ball') {
 
@@ -18,12 +27,12 @@ function onBallClick(elBall) {
             gBall1CurrSize = 100
             elCurrBall.style.height = gBall1CurrSize + 'px'
             elCurrBall.style.width = gBall1CurrSize + 'px'
-            elBallTxt.innerText = gBall1CurrSize
+            elCurrBall.innerText = gBall1CurrSize
         } else {
             gBall1CurrSize += addSize
             elCurrBall.style.height = gBall1CurrSize + 'px'
             elCurrBall.style.width = gBall1CurrSize + 'px'
-            elBallTxt.innerText = gBall1CurrSize
+            elCurrBall.innerText = gBall1CurrSize
         }
 
     } else {
@@ -32,12 +41,61 @@ function onBallClick(elBall) {
             gBall2CurrSize = 100
             elCurrBall.style.height = gBall2CurrSize + 'px'
             elCurrBall.style.width = gBall2CurrSize + 'px'
-            elBallTxt.innerText = gBall1CurrSize
+            elCurrBall.innerText = gBall1CurrSize
         } else {
             gBall2CurrSize += addSize
             elCurrBall.style.height = gBall2CurrSize + 'px'
             elCurrBall.style.width = gBall2CurrSize + 'px'
-            elBallTxt.innerText = gBall2CurrSize
+            elCurrBall.innerText = gBall2CurrSize
         }
     }
+}
+
+
+function onThirdBall() {
+    const elBall1 = document.querySelector('.ball')
+    const elBall2 = document.querySelector('.ball2')
+    if (!gIsBall3Clicked) {
+        elBall1.style.backgroundColor = gCurrBall2Color
+        elBall2.style.backgroundColor = gCurrBall1Color
+        switchBall1AndBall2Size()
+        gIsBall3Clicked = true
+    } else {
+        elBall1.style.backgroundColor = gCurrBall1Color
+        elBall2.style.backgroundColor = gCurrBall2Color
+        switchBall1AndBall2Size()
+        gIsBall3Clicked = false
+    }
+
+}
+
+
+
+function switchBall1AndBall2Size() {
+    const elBall1 = document.querySelector('.ball')
+    const elBall2 = document.querySelector('.ball2')
+
+    console.log('elSpan1:', elSpan1)
+    console.log('elSpan2:', elSpan2)
+    
+    if (!gIsBall3Clicked) {
+        elBall1.style.height = gBall2CurrSize + 'px'
+        elBall1.style.width = gBall2CurrSize + 'px'
+
+        
+        elBall2.style.height = gBall1CurrSize + 'px'
+        elBall2.style.width = gBall1CurrSize + 'px'
+
+    } else {
+        elBall1.style.height = gBall1CurrSize + 'px'
+        elBall1.style.width = gBall1CurrSize + 'px'
+
+        
+        elBall2.style.height = gBall2CurrSize + 'px'
+        elBall2.style.width = gBall2CurrSize + 'px'
+
+    }
+    
+    console.log('elSpan1:', elSpan1)
+    console.log('elSpan2:', elSpan2)
 }
