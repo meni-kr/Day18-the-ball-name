7f2fc9638a06aa6e0c67f55ce283052264d3e968
meni-kr <meni.kramer@gmail.com>
2024-02-13 20:14:16 +0200

    Add get random color to the ball

--- js/util.js
+++ js/util.js
@@ -7,3 +7,13 @@ function getRandomInt(min, max) {
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min) +1) + min
 }
+
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF'
+    var color = '#'
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
