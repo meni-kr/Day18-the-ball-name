cc6a3c4c02d4b8173f651b75dc3baba3b1cc1c96
meni-kr <meni.kramer@gmail.com>
2024-02-13 19:06:47 +0200

    Add more style on the ball

--- js/main.js
+++ js/main.js
@@ -5,9 +5,10 @@ var gBallCurrSize = 100
 function onBallClick(){
     const addSize = 50
     const elBall = document.querySelector('.ball')
+    const elBallTxt = document.querySelector('.ball-text')
     elBall.style.height = gBallCurrSize + addSize + 'px'
     elBall.style.width = gBallCurrSize + addSize + 'px'
     gBallCurrSize +=addSize
-    elBall.innerText = gBallCurrSize
+    elBallTxt.innerText = gBallCurrSize
     
 }
